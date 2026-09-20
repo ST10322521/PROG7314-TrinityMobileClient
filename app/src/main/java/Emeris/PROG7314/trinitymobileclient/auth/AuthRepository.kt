d6efa6f06229cb01ee1607749b4e165f51b1c671
ST10322521 <st10322521@vcconnect.edu.za>
2026-09-20 18:52:24 +0200

    changed AuthRepository.kt to give userId from firebase Auth to use for database user entry

--- app/src/main/java/Emeris/PROG7314/trinitymobileclient/auth/AuthRepository.kt
+++ app/src/main/java/Emeris/PROG7314/trinitymobileclient/auth/AuthRepository.kt
@@ -114,13 +114,20 @@ class AuthRepository(
             }
     }
 
-    /*
+    /**
     * Return current auth used
     *
-    * Returns current [FirebaseUser], or null if no user is authenticated
+    * @returns authenticated user uid [userId], or null if no user is authenticated
      */
-    fun currentUser(): FirebaseUser? {
-        return auth.currentUser
+    fun currentUserId(): String? {
+        val userId = auth.currentUser?.uid
+
+        if (userId != null){
+            Log.d("firebaseAuth", "Authenticated user: $userId")
+        } else {
+            Log.d("firebaseAuth", "No authenticated user found")
+        }
+        return userId
     }
 
     /**
